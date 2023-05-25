4b63220c8987a46299926451e710d7cadbd4daa3
Andy Pickering <anpicker@redhat.com>
2023-05-25 17:25:28 +0900

    Copy console PR #12577 to plugin
    Copy https://github.com/openshift/console/pull/12577 to plugin

--- src/components/silence-form.tsx
+++ src/components/silence-form.tsx
@@ -219,7 +219,11 @@ const SilenceForm_: React.FC<SilenceFormProps> = ({ defaults, history, Info, tit
         history.push(`${SilenceResource.plural}/${encodeURIComponent(silenceID)}`);
       })
       .catch((err) => {
-        setError(_.get(err, 'json.error') || err.message || 'Error saving Silence');
+        const errorMessage =
+          typeof _.get(err, 'json') === 'string'
+            ? _.get(err, 'json')
+            : err.message || 'Error saving Silence';
+        setError(errorMessage);
         setInProgress(false);
       });
   };
