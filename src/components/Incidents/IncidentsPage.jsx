a0febab88754ce8a9be3adc94974635339fe849a
Fewwy <avoznese@redhat.com>
2024-08-08 12:22:08 +0200

    added library for time management and updated fetch

--- src/components/Incidents/IncidentsPage.jsx
+++ src/components/Incidents/IncidentsPage.jsx
@@ -7,12 +7,12 @@ import { getTimeRanges } from '../utils';
 import { parsePrometheusDuration } from '../console/utils/datetime';
 import { getPrometheusURL } from '../console/graphs/helpers';
 import { useDispatch } from 'react-redux';
+import * as _ from 'lodash-es';
 
 const minSamples = 10;
 const maxSamples = 300;
 const minStep = 5 * 1000;
-const getMaxSamplesForSpan = (span) =>
-  _.clamp(Math.round(span / minStep), minSamples, maxSamples);
+const getMaxSamplesForSpan = (span) => _.clamp(Math.round(span / minStep), minSamples, maxSamples);
 const spans = ['1d', '3d', '7d', '15d'];
 
 const IncidentsPage = ({
@@ -21,13 +21,14 @@ const IncidentsPage = ({
   defaultSamples,
   namespace,
   timespan,
-  fixedXDomain
-  }) => {
-  const [startingDate, setStartingDate] = React.useState('')
-  const [endDate, setEndDate] = React.useState('')
-  const [incidentsPageData, setIncidentsPageData] = React.useState([])
+  fixedXDomain,
+}) => {
+  const [startingDate, setStartingDate] = React.useState('');
+  const [endDate, setEndDate] = React.useState('');
+  const [incidentsPageData, setIncidentsPageData] = React.useState([]);
   const defaultSpanText = spans.find((s) => parsePrometheusDuration(s) >= defaultTimespan);
   const [span, setSpan] = React.useState(timespan || parsePrometheusDuration(defaultSpanText));
+  //will be used to define the Xdomain of chart
   const [xDomain, setXDomain] = React.useState();
   const maxSamplesForSpan = defaultSamples || getMaxSamplesForSpan(span);
 
@@ -48,47 +49,42 @@ const IncidentsPage = ({
   const now = Date.now();
   const timeRanges = getTimeRanges(span, endTime || now);
 
-  const queries = ['ALERTS']
+  const queries = ['ALERTS'];
   const queryPromises = _.map(queries, (query) => {
     if (_.isEmpty(query)) {
       return Promise.resolve([]);
     } else {
-      const promiseMap = _.map(
-        timeRanges,
-        (timeRange) =>
-          safeFetch(
-            getPrometheusURL(
-              {
-                endpoint: PrometheusEndpoint.QUERY_RANGE,
-                endTime: timeRange.endTime,
-                namespace,
-                query,
-                samples: Math.ceil(samples / timeRanges.length),
-                timeout: '60s',
-                timespan: timeRange.duration - 1,
-              },
-              customDataSource?.basePath,
-            ),
+      const promiseMap = _.map(timeRanges, (timeRange) =>
+        safeFetch(
+          getPrometheusURL(
+            {
+              endpoint: PrometheusEndpoint.QUERY_RANGE,
+              endTime: timeRange.endTime,
+              namespace,
+              query,
+              samples: Math.ceil(samples / timeRanges.length),
+              timeout: '60s',
+              timespan: timeRange.duration - 1,
+            },
+            customDataSource?.basePath,
           ),
+        ),
       );
       return Promise.all(promiseMap).then((responses) => {
         const results = _.map(responses, 'data.result');
-        const combinedQueries = results.reduce(
-          (accumulator, response) => {
-            response.forEach((metricResult) => {
-              const index = accumulator.findIndex(
-                (item) => JSON.stringify(item.metric) === JSON.stringify(metricResult.metric),
-              );
-              if (index === -1) {
-                accumulator.push(metricResult);
-              } else {
-                accumulator[index].values = accumulator[index].values.concat(metricResult.values);
-              }
-            });
-            return accumulator;
-          },
-          [],
-        );
+        const combinedQueries = results.reduce((accumulator, response) => {
+          response.forEach((metricResult) => {
+            const index = accumulator.findIndex(
+              (item) => JSON.stringify(item.metric) === JSON.stringify(metricResult.metric),
+            );
+            if (index === -1) {
+              accumulator.push(metricResult);
+            } else {
+              accumulator[index].values = accumulator[index].values.concat(metricResult.values);
+            }
+          });
+          return accumulator;
+        }, []);
         // Recombine into the original query to allow for the redux store and the things using
         // it (query duplication, ect) to be able to work. Grab the heading of the first response
         // for the status and structure of the response
@@ -102,73 +98,9 @@ const IncidentsPage = ({
     }
   });
 
-  Promise.all(queryPromises)
-      .then((responses) => {
-        const newResults = _.map(responses, 'data.result');
-        const numDataPoints = _.sumBy(newResults, (r) => _.sumBy(r, 'values.length'));
-
-        if (numDataPoints > maxDataPointsHard && samples === minSamples) {
-          setIsDatasetTooBig(true);
-          return;
-        }
-        setIsDatasetTooBig(false);
-
-        const newSamples = _.clamp(
-          Math.floor((samples * maxDataPointsSoft) / numDataPoints),
-          minSamples,
-          maxSamplesForSpan,
-        );
-
-        // Change `samples` if either
-        //   - It will change by a proportion greater than `samplesLeeway`
-        //   - It will change to the upper or lower limit of its allowed range
-        if (
-          Math.abs(newSamples - samples) / samples > samplesLeeway ||
-          (newSamples !== samples &&
-            (newSamples === maxSamplesForSpan || newSamples === minSamples))
-        ) {
-          setSamples(newSamples);
-        } else {
-          const newGraphData = _.map(
-            newResults,
-            (result, queryIndex) => {
-              return _.map(result, ({ metric, values }) => {
-                // If filterLabels is specified, ignore all series that don't match
-                if (_.some(filterLabels, (v, k) => _.has(metric, k) && metric[k] !== v)) {
-                  return [];
-                } else {
-                  let defaultEmptyValue = null;
-                  if (isStack && _.some(values, (value) => Number.isNaN(Number(value[1])))) {
-                    // eslint-disable-next-line no-console
-                    console.warn(
-                      'Invalid response values for stacked graph converted to 0 for query: ',
-                      queries[queryIndex],
-                    );
-                    defaultEmptyValue = 0;
-                  }
-                  return [metric, formatSeriesValues(values, samples, span, defaultEmptyValue)];
-                }
-              });
-            },
-          );
-          setGraphData(newGraphData);
-
-          _.each(newResults, (r, i) =>
-            dispatch(queryBrowserPatchQuery(i, { series: r ? _.map(r, 'metric') : undefined })),
-          );
-          setUpdating(false);
-        }
-        setError(undefined);
-      })
-      .catch((err) => {
-        if (err.name !== 'AbortError') {
-          setError(err);
-          setUpdating(false);
-        }
-      });
-
-  const dispatch = useDispatch();
-  const queryForAlerts = React.useCallback(() => dispatch(queryBrowserRunQueries()), [dispatch]);
+  Promise.all(queryPromises).then((responses) => {
+    const newResults = _.map(responses, 'data.result');
+  });
 
   return (
     <>
--- src/components/Incidents/utils.js
+++ src/components/Incidents/utils.js
@@ -1,25 +1,32 @@
+import moment from 'moment';
+
 export function processIncidentsTimestamps(data) {
-    return data.map(alert => {
-      // Process each value
-      const processedValues = alert.values.map(value => {
-        const timestamp = value[0];
-        const status = value[1];
+  return data.map((alert) => {
+    // Process each value
+    const processedValues = alert.values.map((value) => {
+      const timestamp = value[0];
+      const status = value[1];
 
-        // Convert timestamp to date
-        const date = new Date(timestamp * 1000);
-        const hours = String(date.getHours()).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = String(date.getFullYear()).slice(-2);
+      // Convert timestamp to date
+      const date = new Date(timestamp * 1000);
+      const hours = String(date.getHours()).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = String(date.getFullYear()).slice(-2);
 
-        // Format date as HH/DD/MM/YY
-        const formattedDate = `${hours}/${day}/${month}/${year}`;
-        return [formattedDate, status];
-      });
-
-      return {
-        ...alert,
-        values: processedValues
-      };
+      // Format date as HH/DD/MM/YY
+      const formattedDate = `${hours}/${day}/${month}/${year}`;
+      return [formattedDate, status];
     });
-  }
+
+    return {
+      ...alert,
+      values: processedValues,
+    };
+  });
+}
+
+const today = moment().startOf('day');
+const threeDaysAgo = moment().subtract(3, 'days');
+const sevenDaysAgo = moment().subtract(7, 'days');
+const fifteenDaysAgo = moment().subtract(15, 'days');
