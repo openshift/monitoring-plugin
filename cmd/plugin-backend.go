f911178dc2288e4e5ce60fd4a106e3a080c9d006
PeterYurkovich <pyurkovi@redhat.com>
2024-09-23 10:47:26 -0400

    Separate alertmanager and thanos-querier parameters in name and namespace

--- cmd/plugin-backend.go
+++ cmd/plugin-backend.go
@@ -11,17 +11,19 @@ import (
 )
 
 var (
-	portArg                  = flag.Int("port", 0, "server port to listen on (default: 9443)")
-	certArg                  = flag.String("cert", "", "cert file path to enable TLS (disabled by default)")
-	keyArg                   = flag.String("key", "", "private key file path to enable TLS (disabled by default)")
-	featuresArg              = flag.String("features", "", "enabled features, comma separated")
-	staticPathArg            = flag.String("static-path", "", "static files path to serve frontend (default: './web/dist')")
-	configPathArg            = flag.String("config-path", "", "config files path (default: './config')")
-	pluginConfigArg          = flag.String("plugin-config-path", "", "plugin yaml configuration")
-	logLevelArg              = flag.String("log-level", "error", "verbosity of logs\noptions: ['panic', 'fatal', 'error', 'warn', 'info', 'debug', 'trace']\n'trace' level will log all incoming requests\n(default 'error')")
-	alertmanagerLocationArg  = flag.String("alertmanager-location", "", "alert manager location for acm mode (format 'name/namespace'")
-	thanosQuerierLocationArg = flag.String("thanos-querier-location", "", "thanos querier location for acm mode (format 'name/namespace'")
-	log                      = logrus.WithField("module", "main")
+	portArg                   = flag.Int("port", 0, "server port to listen on (default: 9443)")
+	certArg                   = flag.String("cert", "", "cert file path to enable TLS (disabled by default)")
+	keyArg                    = flag.String("key", "", "private key file path to enable TLS (disabled by default)")
+	featuresArg               = flag.String("features", "", "enabled features, comma separated")
+	staticPathArg             = flag.String("static-path", "", "static files path to serve frontend (default: './web/dist')")
+	configPathArg             = flag.String("config-path", "", "config files path (default: './config')")
+	pluginConfigArg           = flag.String("plugin-config-path", "", "plugin yaml configuration")
+	logLevelArg               = flag.String("log-level", "error", "verbosity of logs\noptions: ['panic', 'fatal', 'error', 'warn', 'info', 'debug', 'trace']\n'trace' level will log all incoming requests\n(default 'error')")
+	alertmanagerNameArg       = flag.String("alertmanager-name", "", "alertmanager name for acm mode")
+	alertmanagerNamespaceArg  = flag.String("alertmanager-namespace", "", "alertmanager namespace for acm mode")
+	thanosQuerierNameArg      = flag.String("thanos-querier-name", "", "thanos querier name for acm mode")
+	thanosQuerierNamespaceArg = flag.String("thanos-querier-namespace", "", "thanos querier namespace for acm mode")
+	log                       = logrus.WithField("module", "main")
 )
 
 func main() {
@@ -35,8 +37,10 @@ func main() {
 	configPath := mergeEnvValue("MONITORING_PLUGIN_MANIFEST_CONFIG_PATH", *configPathArg, "/opt/app-root/web/dist")
 	pluginConfigPath := mergeEnvValue("MONITORING_PLUGIN_CONFIG_PATH", *pluginConfigArg, "/etc/plugin/config.yaml")
 	logLevel := mergeEnvValue("MONITORING_PLUGIN_LOG_LEVEL", *logLevelArg, "error")
-	alertmanagerLocation := mergeEnvValue("MONITORING_PLUGIN_ALERTMANAGER_LOCATION", *alertmanagerLocationArg, "")
-	thanosQuerierLocation := mergeEnvValue("MONITORING_PLUGIN_THANOS_QUERIER_LOCATION", *thanosQuerierLocationArg, "")
+	alertmanagerName := mergeEnvValue("MONITORING_PLUGIN_ALERTMANAGER_NAME", *alertmanagerNameArg, "")
+	alertmanagerNamespace := mergeEnvValue("MONITORING_PLUGIN_ALERTMANAGER_NAMESPACE", *alertmanagerNamespaceArg, "")
+	thanosQuerierName := mergeEnvValue("MONITORING_PLUGIN_THANOS_QUERIER_NAME", *thanosQuerierNameArg, "")
+	thanosQuerierNamespace := mergeEnvValue("MONITORING_PLUGIN_THANOS_QUERIER_NAMESPACE", *thanosQuerierNamespaceArg, "")
 
 	featuresList := strings.Fields(strings.Join(strings.Split(strings.ToLower(features), ","), " "))
 
@@ -48,16 +52,18 @@ func main() {
 	log.Infof("enabled features: %+q\n", featuresList)
 
 	server.Start(&server.Config{
-		Port:                  port,
-		CertFile:              cert,
-		PrivateKeyFile:        key,
-		Features:              featuresSet,
-		StaticPath:            staticPath,
-		ConfigPath:            configPath,
-		PluginConfigPath:      pluginConfigPath,
-		LogLevel:              logLevel,
-		ThanosQuerierLocation: thanosQuerierLocation,
-		AlertmanagerLocation:  alertmanagerLocation,
+		Port:                   port,
+		CertFile:               cert,
+		PrivateKeyFile:         key,
+		Features:               featuresSet,
+		StaticPath:             staticPath,
+		ConfigPath:             configPath,
+		PluginConfigPath:       pluginConfigPath,
+		LogLevel:               logLevel,
+		AlertmanagerName:       alertmanagerName,
+		AlertmanagerNamespace:  alertmanagerNamespace,
+		ThanosQuerierName:      thanosQuerierName,
+		ThanosQuerierNamespace: thanosQuerierNamespace,
 	})
 }
 
