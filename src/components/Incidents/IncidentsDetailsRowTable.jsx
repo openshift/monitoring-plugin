ae88ddc9d09e50ada9b48da38336f76a3fd3fe5c
Fewwy <avoznese@redhat.com>
2024-10-07 19:55:22 +0200

    finished expanded details

--- src/components/Incidents/IncidentsDetailsRowTable.jsx
+++ src/components/Incidents/IncidentsDetailsRowTable.jsx
@@ -1,26 +1,76 @@
 import React from 'react';
 import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
+import { GreenCheckCircleIcon } from '@openshift-console/dynamic-plugin-sdk';
+import { BellIcon, ExclamationCircleIcon, InfoCircleIcon } from '@patternfly/react-icons';
+import ExclamationTriangleIcon from '@patternfly/react-icons/dist/esm/icons/exclamation-triangle-icon';
+import { Icon } from '@patternfly/react-core';
+import { Link } from 'react-router-dom';
+import { AlertResource } from '../utils';
+import { MonitoringResourceIcon } from '../alerting/AlertUtils';
 
 const IncidentsDetailsRowTable = ({ alerts }) => {
   return (
     <Table borders={'compactBorderless'}>
       <Thead>
         <Tr>
-          <Th width={40}>Alert name</Th>
-          <Th width={25}>Namespace</Th>
-          <Th width={25}>Severity</Th>
-          <Th width={25}>State</Th>
-          <Th width={40}>Firing started</Th>
-          <Th width={40}>Firing ended</Th>
+          <Th width={25}>Alert name</Th>
+          <Th width={15}>Namespace</Th>
+          <Th width={10}>Severity</Th>
+          <Th width={10}>State</Th>
+          <Th width={20}>Firing started</Th>
+          <Th width={20}>Firing ended</Th>
         </Tr>
       </Thead>
       <Tbody>
         {alerts?.map((alertDetails, rowIndex) => (
           <Tr key={rowIndex}>
-            <Td dataLabel="expanded-details-alertname">{alertDetails.alertname}</Td>
+            <Td dataLabel="expanded-details-alertname">
+              <MonitoringResourceIcon resource={AlertResource} />
+              <Link
+                // eslint-disable-next-line max-len
+                to={`/monitoring/alerts/4033586302?namespace=${alertDetails.namespace}&prometheus=${alertDetails.namespace}&severity=${alertDetails.severity}&alertname=${alertDetails.alertname}`}
+              >
+                {alertDetails.alertname}
+              </Link>
+            </Td>
             <Td dataLabel="expanded-details-namespace">{alertDetails.namespace}</Td>
-            <Td dataLabel="expanded-details-severity">{alertDetails.severity}</Td>
-            <Td dataLabel="expanded-details-alertstate">{alertDetails.alertstate}</Td>
+            <Td dataLabel="expanded-details-severity">
+              {alertDetails.severity === 'critical' ? (
+                <>
+                  <Icon status="danger">
+                    <ExclamationCircleIcon />
+                  </Icon>
+                  Critical
+                </>
+              ) : alertDetails.severity === 'warning' ? (
+                <>
+                  <Icon status="warning">
+                    <ExclamationTriangleIcon />
+                  </Icon>
+                  Warning
+                </>
+              ) : (
+                <>
+                  <Icon status="info">
+                    <InfoCircleIcon />
+                  </Icon>
+                  Info
+                </>
+              )}
+            </Td>
+            <Td dataLabel="expanded-details-alertstate">
+              {alertDetails.alertstate === 'firing' ? (
+                <>
+                  <BellIcon />
+                  Firing
+                </>
+              ) : (
+                <>
+                  <GreenCheckCircleIcon />
+                  Resolved
+                </>
+              )}
+            </Td>
             <Td dataLabel="expanded-details-firingstart">{Date(alertDetails.alertsStartFiring)}</Td>
             <Td dataLabel="expanded-details-firingend">{Date(alertDetails.alertsEndFiring)}</Td>
           </Tr>
--- src/components/Incidents/IncidentsTable.jsx
+++ src/components/Incidents/IncidentsTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Table, Thead, Tr, Th, Tbody, Td, ExpandableRowContent } from '@patternfly/react-table';
-import { Bullseye, Checkbox, Label, Spinner } from '@patternfly/react-core';
+import { Bullseye, Card, CardBody, Checkbox, Label, Spinner } from '@patternfly/react-core';
 import InfoCircleIcon from '@patternfly/react-icons/dist/esm/icons/info-circle-icon';
 import { AlertStateIcon } from '../alerting/AlertUtils';
 import IncidentsDetailsRowTable from './IncidentsDetailsRowTable';
@@ -21,83 +21,81 @@ export const IncidentsTable = ({ loaded, data = [] }) => {
   const isAlertExpanded = (alert) => expandedAlerts.includes(alert.component);
 
   return (
-    <React.Fragment>
-      <Table aria-label="alerts-table" isExpandable>
-        <Thead>
-          <Tr>
-            <Th screenReaderText="Row expansion" />
-            <Th width={10}>{columnNames.checkbox}</Th>
-            <Th width={25}>{columnNames.component}</Th>
-            <Th width={25}>{columnNames.severity}</Th>
-            <Th width={25}>{columnNames.state}</Th>
-          </Tr>
-        </Thead>
-        {!loaded ? (
-          <Bullseye>
-            <Spinner aria-label="incidents-chart-spinner" />
-          </Bullseye>
-        ) : (
-          data.map((alert, rowIndex) => {
-            return (
-              <Tbody key={rowIndex} isExpanded={isAlertExpanded(alert)}>
-                <Tr>
-                  <Td
-                    expand={
-                      alert.alertsExpandedRowData
-                        ? {
-                            rowIndex,
-                            isExpanded: isAlertExpanded(alert),
-                            onToggle: () => setAlertExpanded(alert, !isAlertExpanded(alert)),
-                            expandId: 'alert-expandable',
-                          }
-                        : undefined
-                    }
-                  />
-                  <Td>
-                    <Checkbox />
-                  </Td>
-                  <Td dataLabel={columnNames.component}>{alert.component}</Td>
-                  <Td>
-                    {alert.critical > 0 ? (
-                      <Label color="red" icon={<InfoCircleIcon />}>
-                        {alert.critical}
-                      </Label>
-                    ) : (
-                      ''
-                    )}
-                    {alert.warning > 0 ? (
-                      <Label color="gold" icon={<InfoCircleIcon />}>
-                        {alert.warning}
-                      </Label>
-                    ) : (
-                      ''
-                    )}
-                    {alert.info > 0 ? (
-                      <Label color="blue" icon={<InfoCircleIcon />}>
-                        {alert.info}
-                      </Label>
-                    ) : (
-                      ''
-                    )}
-                  </Td>
-                  <Td dataLabel={columnNames.state}>
-                    <AlertStateIcon state={alert.alertstate} />
-                  </Td>
-                </Tr>
-                {alert.alertsExpandedRowData && (
-                  <Tr isExpanded={isAlertExpanded(alert)}>
-                    <Td width={100} colSpan={6}>
-                      <ExpandableRowContent>
-                        <IncidentsDetailsRowTable alerts={alert.alertsExpandedRowData} />
-                      </ExpandableRowContent>
+    <Card>
+      <CardBody>
+        <Table aria-label="alerts-table" isExpandable>
+          <Thead>
+            <Tr>
+              <Th screenReaderText="Row expansion" />
+              <Th width={50}>{columnNames.component}</Th>
+              <Th width={25}>{columnNames.severity}</Th>
+              <Th width={25}>{columnNames.state}</Th>
+            </Tr>
+          </Thead>
+          {!loaded ? (
+            <Bullseye>
+              <Spinner aria-label="incidents-chart-spinner" />
+            </Bullseye>
+          ) : (
+            data.map((alert, rowIndex) => {
+              return (
+                <Tbody key={rowIndex} isExpanded={isAlertExpanded(alert)}>
+                  <Tr>
+                    <Td
+                      expand={
+                        alert.alertsExpandedRowData
+                          ? {
+                              rowIndex,
+                              isExpanded: isAlertExpanded(alert),
+                              onToggle: () => setAlertExpanded(alert, !isAlertExpanded(alert)),
+                              expandId: 'alert-expandable',
+                            }
+                          : undefined
+                      }
+                    />
+                    <Td dataLabel={columnNames.component}>{alert.component}</Td>
+                    <Td>
+                      {alert.critical > 0 ? (
+                        <Label color="red" icon={<InfoCircleIcon />}>
+                          {alert.critical}
+                        </Label>
+                      ) : (
+                        ''
+                      )}
+                      {alert.warning > 0 ? (
+                        <Label color="gold" icon={<InfoCircleIcon />}>
+                          {alert.warning}
+                        </Label>
+                      ) : (
+                        ''
+                      )}
+                      {alert.info > 0 ? (
+                        <Label color="blue" icon={<InfoCircleIcon />}>
+                          {alert.info}
+                        </Label>
+                      ) : (
+                        ''
+                      )}
+                    </Td>
+                    <Td dataLabel={columnNames.state}>
+                      <AlertStateIcon state={alert.alertstate} />
                     </Td>
                   </Tr>
-                )}
-              </Tbody>
-            );
-          })
-        )}
-      </Table>
-    </React.Fragment>
+                  {alert.alertsExpandedRowData && (
+                    <Tr isExpanded={isAlertExpanded(alert)}>
+                      <Td width={100} colSpan={6}>
+                        <ExpandableRowContent>
+                          <IncidentsDetailsRowTable alerts={alert.alertsExpandedRowData} />
+                        </ExpandableRowContent>
+                      </Td>
+                    </Tr>
+                  )}
+                </Tbody>
+              );
+            })
+          )}
+        </Table>
+      </CardBody>
+    </Card>
   );
 };
