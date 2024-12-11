fe5d3b28ec87e1e4c19ae655ec209f79dd32c374
PeterYurkovich <pyurkovi@redhat.com>
2024-12-11 10:21:02 -0500

    NO-JIRA: Remove duplicate parameter

--- web/src/components/dashboards/graph.tsx
+++ web/src/components/dashboards/graph.tsx
@@ -67,7 +67,6 @@ const Graph: React.FC<Props> = ({
       showLegend={showLegend}
       timespan={timespan}
       units={units}
-      namespace={namespace}
       onDataChange={onDataChange}
     />
   );
