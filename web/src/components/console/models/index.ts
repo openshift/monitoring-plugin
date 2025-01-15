14edab7bfb6c84388d4d32de7310dffe17040bcd
PeterYurkovich <pyurkovi@redhat.com>
2025-01-15 11:54:27 -0500

    Remove default translation namespace

--- web/src/components/console/models/index.ts
+++ web/src/components/console/models/index.ts
@@ -65,14 +65,14 @@ export const NodeModel = {
 export const NamespaceModel: K8sModel = {
   apiVersion: 'v1',
   label: 'Namespace',
-  // t('public~Namespace')
+  // t('Namespace')
   labelKey: 'public~Namespace',
   plural: 'namespaces',
   abbr: 'NS',
   kind: 'Namespace',
   id: 'namespace',
   labelPlural: 'Namespaces',
-  // t('public~Namespaces')
+  // t('Namespaces')
   labelPluralKey: 'public~Namespaces',
 };
 
@@ -80,14 +80,14 @@ export const ProjectModel: K8sModel = {
   apiVersion: 'v1',
   apiGroup: 'project.openshift.io',
   label: 'Project',
-  // t('public~Project')
+  // t('Project')
   labelKey: 'public~Project',
   plural: 'projects',
   abbr: 'PR',
   kind: 'Project',
   id: 'project',
   labelPlural: 'Projects',
-  // t('public~Projects')
+  // t('Projects')
   labelPluralKey: 'public~Projects',
 };
 
