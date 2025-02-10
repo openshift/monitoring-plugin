8f5013271a4d89db523b8c64d033f43cfd9a13b8
Jenny Zhu <jenny.a.zhu@gmail.com>
2025-02-10 17:16:49 -0500

    stash

--- web/src/components/alerting/AlertingPage.tsx
+++ web/src/components/alerting/AlertingPage.tsx
@@ -7,12 +7,17 @@ import '../_monitoring.scss';
 import {
   getAlertRulesUrl,
   getAlertsUrl,
+  getIncidentsUrl,
   getSilencesUrl,
   usePerspective,
 } from '../hooks/usePerspective';
 import AlertsPage from '../alerting/AlertsPage';
 import SilencesPage from '../alerting/SilencesPage';
 import AlertRulesPage from '../alerting/AlertRulesPage';
+import { useFeatures } from '../hooks/useFeatures';
+import incidentsPageWithFallback from '../Incidents/IncidentsPage';
+import { HelloWorld } from './HelloWorld';
+import { HorizontalNav } from '@openshift-console/dynamic-plugin-sdk';
 
 const Tab: React.FC<{ active: boolean; children: React.ReactNode }> = ({ active, children }) => (
   <li
@@ -27,12 +32,22 @@ const Tab: React.FC<{ active: boolean; children: React.ReactNode }> = ({ active,
 const AlertingPage: React.FC<RouteComponentProps<{ url: string }>> = ({ match }) => {
   const { t } = useTranslation(process.env.I18N_NAMESPACE);
   const { perspective } = usePerspective();
+  const { areIncidentsActive } = useFeatures();
 
   const alertsPath = getAlertsUrl(perspective);
   const rulesPath = getAlertRulesUrl(perspective);
   const silencesPath = getSilencesUrl(perspective);
+  const incidentsPath = getIncidentsUrl(perspective);
 
   const { url } = match;
+
+  if (areIncidentsActive) {
+    console.log('JZ areIncidentsActive: ', areIncidentsActive);
+    console.log('JZ incidentsPath: ', incidentsPath);
+    console.log('JZ alertsPath: ', alertsPath);
+  }
+
+  const helloPath = '/monitoring/hello';
 
   return (
     <>
@@ -55,11 +70,21 @@ const AlertingPage: React.FC<RouteComponentProps<{ url: string }>> = ({ match })
         <Tab active={url === rulesPath}>
           <Link to={rulesPath}>{t('Alerting rules')}</Link>
         </Tab>
+        <Tab active={url === helloPath}>
+          <Link to={helloPath}> HELLO </Link>
+        </Tab>
+        {areIncidentsActive && (
+          <Tab active={url === incidentsPath}>
+            <Link to={incidentsPath}>Incidents</Link>
+          </Tab>
+        )}
       </ul>
       <Switch>
         <Route path={alertsPath} exact component={AlertsPage} />
         <Route path={rulesPath} exact component={AlertRulesPage} />
         <Route path={silencesPath} exact component={SilencesPage} />
+        <Route path={silencesPath} exact component={incidentsPageWithFallback} />
+        <Route path={helloPath} exact component={AlertsPage} />
       </Switch>
     </>
   );
---
+++ web/src/components/alerting/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HelloWorld = () => {
+  return (
+    <>
+      <h1> hello world </h1>
+    </>
+  );
+};
