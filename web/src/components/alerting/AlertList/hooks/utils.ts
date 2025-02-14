ac2395726a25314474a2ee0a65f064d96ff50a9d
Ugo Palatucci <upalatuc@redhat.com>
2025-02-14 10:31:51 +0100

    CNV-54399: Aggregate alert list

---
+++ web/src/components/alerting/AlertList/hooks/utils.ts
@@ -0,0 +1,33 @@
+import { Alert } from '@openshift-console/dynamic-plugin-sdk';
+import { FILTER_TYPES, SelectedFilters } from '../../useSelectedFilters';
+import { alertSource } from '../../AlertUtils';
+
+export const alertFilters: Record<string, (selectedInput: string[], alert: Alert) => boolean> = {
+  [FILTER_TYPES.ALERT_STATE]: (selectedInput, obj) => {
+    if (!selectedInput) return true;
+    return selectedInput.some((selectedState) => obj.state === selectedState);
+  },
+  [FILTER_TYPES.ALERT_SOURCE]: (selectedInput, obj) => {
+    if (!selectedInput) return true;
+    return selectedInput.some((selectedSource) => alertSource(obj) === selectedSource);
+  },
+  [FILTER_TYPES.ALERT_CLUSTER]: (selectedInput, obj) => {
+    if (!selectedInput) return true;
+    return selectedInput.some((selectedCluster) => obj.labels?.cluster === selectedCluster);
+  },
+} as const;
+
+export const filterAlerts = (alerts: Alert[], selectedFilters: SelectedFilters) => {
+  if (!Object.keys(selectedFilters).length) return alerts;
+
+  return alerts.filter((iface) =>
+    Object.keys(selectedFilters).every((filterType) => {
+      const selectedValues = selectedFilters[filterType];
+      const filter = alertFilters[filterType];
+
+      if (!filter) return true;
+
+      return filter(selectedValues, iface);
+    }),
+  );
+};
