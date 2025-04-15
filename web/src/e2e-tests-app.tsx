a941d271b86b4d614a68f07ed22d270bb6987831
Jenny Zhu <jenny.a.zhu@gmail.com>
2025-04-15 13:00:42 -0400

    chore: update react-router-dom with react-router-dom-v5-compat in router.tsx and e2e-test-app.tsx

--- web/src/e2e-tests-app.tsx
+++ web/src/e2e-tests-app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Link } from 'react-router-dom';
 import { combineReducers, createStore } from 'redux';
 
 import ObserveReducers, { ObserveState } from './reducers/observe';
@@ -9,7 +8,7 @@ import MonitoringRouter from './components/router';
 import i18n from './i18n';
 
 import '@patternfly/patternfly/patternfly.css';
-import { CompatRouter } from 'react-router-dom-v5-compat';
+import { BrowserRouter, Link } from 'react-router-dom-v5-compat';
 
 type RootState = { observe: ObserveState };
 
@@ -20,16 +19,14 @@ const store = createStore(combineReducers<RootState>(baseReducers), {});
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <CompatRouter>
-        <div id="page-sidebar">
-          Observe
-          <Link to="/monitoring/alerts">Alerting</Link>
-          <Link to="/monitoring/query-browser">Metrics</Link>
-          <Link to="/monitoring/dashboards">Dashboards</Link>
-          <Link to="/monitoring/targets">Targets</Link>
-        </div>
-        <MonitoringRouter />
-      </CompatRouter>
+      <div id="page-sidebar">
+        Observe
+        <Link to="/monitoring/alerts">Alerting</Link>
+        <Link to="/monitoring/query-browser">Metrics</Link>
+        <Link to="/monitoring/dashboards">Dashboards</Link>
+        <Link to="/monitoring/targets">Targets</Link>
+      </div>
+      <MonitoringRouter />
     </BrowserRouter>
   </Provider>
 );
