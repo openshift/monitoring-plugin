e4fdb90bf4ee3005b568e43a2561e906681f14d1
Jenny Zhu <jenny.a.zhu@gmail.com>
2025-04-15 11:32:47 -0400

    chore: npm install react-router-dom-v5-compat, update with <CompatRouter>
    Signed-off-by: Jenny Zhu <jenny.a.zhu@gmail.com>

--- web/src/e2e-tests-app.tsx
+++ web/src/e2e-tests-app.tsx
@@ -9,6 +9,7 @@ import MonitoringRouter from './components/router';
 import i18n from './i18n';
 
 import '@patternfly/patternfly/patternfly.css';
+import { CompatRouter } from 'react-router-dom-v5-compat';
 
 type RootState = { observe: ObserveState };
 
@@ -19,14 +20,16 @@ const store = createStore(combineReducers<RootState>(baseReducers), {});
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div id="page-sidebar">
-        Observe
-        <Link to="/monitoring/alerts">Alerting</Link>
-        <Link to="/monitoring/query-browser">Metrics</Link>
-        <Link to="/monitoring/dashboards">Dashboards</Link>
-        <Link to="/monitoring/targets">Targets</Link>
-      </div>
-      <MonitoringRouter />
+      <CompatRouter>
+        <div id="page-sidebar">
+          Observe
+          <Link to="/monitoring/alerts">Alerting</Link>
+          <Link to="/monitoring/query-browser">Metrics</Link>
+          <Link to="/monitoring/dashboards">Dashboards</Link>
+          <Link to="/monitoring/targets">Targets</Link>
+        </div>
+        <MonitoringRouter />
+      </CompatRouter>
     </BrowserRouter>
   </Provider>
 );
