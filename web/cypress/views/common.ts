dad7bec008eefe2c2a0a443700f187275644eac2
Evelyn Tanigawa Murasaki <emurasak@redhat.com>
2025-05-12 07:10:43 -0300

    implementing monitoring plugin and monitoring console plugin BVT
    - based on distributed tracing ui testing for monitoring console plugin (coo_bvt)
    - created monitoring plugin ui testing (bvt)
    - changing go-builder from brew to registry.ci on Dockerfile.mcp
    - created Dockerfile.dev-mcp
    - included Dockerfile.dev-mcp into Makefile
    - README.md to document cypress testing

--- web/cypress/views/common.ts
+++ web/cypress/views/common.ts
@@ -1,3 +1,78 @@
-export const projectDropdown = {
-  shouldNotExist: () => cy.byLegacyTestID('namespace-bar-dropdown').should('not.exist'),
-};
+export const commonPages = {
+  projectDropdownShouldNotExist: () => cy.byLegacyTestID('namespace-bar-dropdown').should('not.exist'),
+  projectDropdownShouldExist: () => cy.byLegacyTestID('namespace-bar-dropdown').should('exist'),
+  titleShouldHaveText: (title: string) => {
+    cy.log('commonPages.titleShouldHaveText - ' + `${title}`);
+    cy
+    .byClass('pf-v6-c-title pf-m-h1')
+    .contains(title)
+    .should('exist');
+  },
+  //Targets page
+  cmo_titleShouldHaveText: (title: string) => {
+    cy.log('commonPages.cmo_titleShouldHaveText - ' + `${title}`);
+    cy
+      .byClass('pf-v6-c-content--h1')
+      .contains(title)
+      .should('exist');
+  },
+  linkShouldExist:(linkName: string) =>{
+    cy.log('commonPages.linkShouldExist - ' + `${linkName}`);
+    cy
+      .byClass('pf-v6-c-button__text')
+      .contains(linkName)
+      .should('exist');
+  },
+  clickBellIcon: () => {
+    cy.log('commonPages.clickBellIcon');
+    cy
+      .byClass('pf-v6-c-button pf-m-stateful pf-m-plain co-masthead-button')
+      .click();
+  },
+    
+  bellIconClickAlert: (alert: string) => {
+    cy.log('commonPages.bellIconClickAlert');
+    cy
+      .byClass('pf-v6-c-notification-drawer__list-item-header')
+      .contains(alert)
+      .click();
+  },
+
+  detailsPage: {
+    alert: (alert: string) => {
+      cy.log('commonPages.detailsPage.alert');
+      cy.byClass('co-m-resource-icon co-m-resource-alert').contains('A').should('be.visible');
+      cy.byOUIAID('OUIA-Generated-Button-primary').contains('Silence alert').should('be.visible');
+      cy.byClass('co-m-resource-icon co-m-resource-alertrule').scrollIntoView();
+      cy.byClass('co-m-resource-icon co-m-resource-alertrule').contains('AR').should('be.visible');
+      cy.byTestID('alert-rules-detail-resource-link').contains(alert).should('be.visible');
+
+    },
+    alertRule: () => {
+      cy.log('commonPages.detailsPage.alertRule');
+      cy.byClass('co-m-resource-icon co-m-resource-alertrule').contains('AR').should('be.visible');
+      cy.byClass('pf-v6-c-code-block__content').should('be.visible');
+      cy.byTestID('active-alerts').scrollIntoView();
+      cy.byTestID('active-alerts').should('have.length.at.least', 1);
+    },
+    common: (alert: string, severity: string) => {
+      cy.log('commonPages.detailsPage.common');
+      commonPages.titleShouldHaveText(alert);
+      //2 badges and 1 label severity=Critical
+      // cy.byClass('pf-v6-c-label__text').filter(`:contains("${severity}")`).should('be.greaterThan', 1);
+      cy.byOUIAID('OUIA-Generated-Button-link').contains('graph').should('be.visible');
+      // cy.byOUIAID('OUIA-Generated-TextInputBase').should('not.be.empty');
+      cy.byOUIAID('OUIA-Generated-Button-tertiary').contains('Reset zoom').should('be.visible');
+      cy.get(`[aria-label="Inspect"]`).should('be.visible');
+      cy.byClass('pf-v6-c-chart').should('be.visible');
+    },
+    administration_clusterSettings: () => {
+      cy.log('commonPages.detailsPage.administration_clusterSettings');
+      cy.byLegacyTestID('horizontal-link-Configuration').click();
+      cy.byLegacyTestID('item-filter', {timeout: 250000}).should('be.visible').type('Alertmanager')
+      cy.byLegacyTestID('Alertmanager').should('be.visible').click();
+      cy.byLegacyTestID('create-receiver').should('be.visible');
+    },
+  }
+
+}
--- web/cypress/views/list-page.ts
+++ web/cypress/views/list-page.ts
@@ -1,24 +1,205 @@
 export const listPage = {
-  titleShouldHaveText: (title: string) =>
-    cy
-      .byLegacyTestID('resource-title')
-      .contains(title)
-      .should('exist'),
-  filter: {
-    byName: (name: string) => {
-      cy.byTestID('name-filter-input')
-        .clear()
-        .type(name);
-    },
+
+  tableShoulBeLoaded: () => {
+    cy.get('[id="silences-table-scroll"]').should('be.visible');
   },
-  rows: {
+ 
+  tabShouldHaveText: (tab: string) =>{
+    cy.log('listPage.tabShouldHaveText');
+    cy
+    .byClass('pf-v6-c-tabs__item-text')
+    .contains(tab)
+    .should('exist');
+  },
+
+  filter: {
+    /**
+     * 
+     * @param tab alerts-tab, silences, alerting-rules 
+     * @param name 
+     */
+    byName: (tab: string, name: string) => {
+      cy.log('listPage.filter.byName');
+      try {
+        cy.get(`[id="${tab}-content"]`).find('input[data-test="name-filter-input"]')
+        .as('input').should('be.visible');
+        cy.get('@input', { timeout: 10000 }).type(name + '{enter}').should('have.attr', 'value', name);
+      }
+      catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+    },
+    /**
+     * 
+     * @param tab alerts-tab, silences, alerting-rules 
+     * @param label 
+     */
+    byLabel: (tab: string, label: string) => {
+      cy.log('listPage.filter.byLabel');
+      cy.byTestID('dropdown-button').click();
+      cy.byTestID('dropdown-menu').contains('Label').click();
+      cy.get(`[id="${tab}-content"]`).find('[data-test="name-filter-input"]')
+      .should('be.visible')
+      .type(label);
+    },
+    /**
+     * 
+     * @param tab alerts-tab, silences, alerting-rules 
+     */
+    clearAllFilters: (tab: string,) => {
+      cy.log('listPage.filter.clearAllFilters');
+      cy.get(`[id="${tab}-content"]`).find('[class="pf-v6-c-button__text"]')
+        .contains('Clear all filters')
+        .should('be.visible');
+      try {
+        cy.get(`[id="${tab}-content"]`).find('[class="pf-v6-c-button__text"]')
+          .contains('Clear all filters')
+          .click();
+      } catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+    },
+    clickFilter:(toOpen: boolean)=>{
+      cy.log('listPage.filter.clickFilter');
+      if (toOpen){
+        cy.byClass('pf-v6-c-menu-toggle').eq(0).click().should('have.class', 'pf-v6-c-menu-toggle pf-m-expanded');
+      }else{
+        cy.byClass('pf-v6-c-menu-toggle').eq(0).click().should('not.have.class', 'pf-m-expanded');
+      }
+    },
+    /**
+     * 
+     * @param open true = open, false = nothing
+     * @param option 
+     * @returns 
+     */
+    selectFilterOption:(open: boolean, option: string, close: boolean)=> {
+      cy.log('listPage.filter.selectFilterOption');
+      if (open){
+        listPage.filter.clickFilter(open);
+      };
+      cy.byClass('co-filter-dropdown-item__name').contains(option).click();
+      if (close){
+        listPage.filter.clickFilter(false);
+      };
+    },
+      
+  },
+  ARRows: {
     shouldBeLoaded: () => {
-      cy.get(`[data-test-rows="resource-row"`).should('be.visible');
+      cy.log('listPage.ARRows.shouldBeLoaded');
+      cy.byOUIAID('OUIA-Generated-Table').should('be.visible');
+      // cy.get(`[data-test-rows="resource-row"`).should('be.visible');
     },
     countShouldBe: (count: number) => {
-      cy.get(`[data-test-rows="resource-row"`).should('have.length', count);
+      cy.log('listPage.ARRows.countShouldBe');
+      cy.byClass('pf-v6-c-table__tbody').should('have.length', count);
+      // cy.get(`[data-test-rows="resource-row"`).should('have.length', count);
     },
-    shouldExist: (resourceName: string) =>
-      cy.get(`[data-test-rows="resource-row"]`).contains(resourceName),
+    ARShouldBe: (alert: string, severity: string, total: number, state: string) => {
+      cy.log('listPage.ARRows.ARShouldBe');
+      cy.byOUIAID('OUIA-Generated-Button-plain').should('exist');
+      cy.byClass('co-m-resource-icon co-m-resource-alertrule').contains('AR');
+      cy.byClass('pf-v6-c-table__td').contains(alert).should('exist');
+      cy.byClass('pf-v6-c-label__text').contains(severity).should('exist');
+      cy.byClass('pf-v6-c-badge pf-m-read').contains(total).should('exist');
+      cy.byClass('pf-v6-c-table__td').contains(state).should('exist');
+    },
+    AShouldBe: (alert: string, severity: string, namespace: string)  => {
+      cy.log('listPage.ARRows.AShouldBe');
+      cy.byClass('co-m-resource-icon co-m-resource-alert').should('exist');
+      cy.byClass('pf-v6-l-flex pf-m-space-items-none pf-m-nowrap').contains(alert).should('exist');
+      cy.byClass('pf-v6-c-label__text').contains(severity).should('exist');
+      cy.byClass('co-resource-item__resource-name').contains(namespace).should('exist');
+    },
+    expandRow:() => {
+      cy.log('listPage.ARRows.expandRow');
+      try {
+          cy.get('body').then( ($provider) => {
+          if ($provider.find('button[class="pf-v6-c-button pf-m-plain pf-m-expanded"]').length > 0) {
+              cy.log('Already expanded');
+          } else{
+              cy.get('button[class="pf-v6-c-button pf-m-plain"]', { timeout: 10000 })
+              .eq(2)
+              .click()
+              .should('have.class', 'pf-m-expanded');
+          }
+        })
+      } catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+    },
+    clickAlertingRule: () => {
+      cy.log('listPage.ARRows.clickAlertingRule');
+      try {
+          cy.byClass('co-m-resource-icon co-m-resource-alertrule')
+            .contains('AR')
+            .parent()
+            .next()
+            .should('be.visible')
+            .click();
+      } catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+     
+    },
+    clickAlert: () => {
+      cy.log('listPage.ARRows.clickAlert');
+      try {
+      cy.get('[class="co-m-resource-icon co-m-resource-alert"]')
+        .parent()
+        .next('div')
+        .click();
+      } catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+      
+    },
+    clickAlertKebab: () => {
+      cy.log('listPage.ARRows.clickAlertKebab');
+      try {
+        cy.byLegacyTestID('kebab-button').should('be.visible').click();
+      }catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+    },
+    silenceAlert:() => {
+      cy.log('listPage.ARRows.silentAlert');
+      try {
+        listPage.ARRows.clickAlertKebab();
+        cy.byClass('pf-v6-c-menu__item-text').contains('Silence alert').should('be.visible').click();
+      } catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+      
+
+    },
+    editAlert:() => {
+      cy.log('listPage.ARRows.silentAlert');
+      try {
+        listPage.ARRows.clickAlertKebab();
+        cy.byClass('pf-v6-c-menu__item-text').contains('Edit alert').should('be.visible').click();
+      }catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+    },
+    expireAlert:() => {
+      cy.log('listPage.ARRows.silentAlert');
+      try {
+        listPage.ARRows.clickAlertKebab();
+        cy.byClass('pf-v6-c-menu__item-text').contains('Expire alert').should('be.visible').click();
+      }catch (error) {
+        cy.log(`${error.message}`);
+        throw error; 
+      }
+    }
   },
 };
