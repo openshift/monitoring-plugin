096eb06a320f85aa1e3ee218d86cfaa354ad97a0
Gabriel Bernal <gbernal@redhat.com>
2025-06-13 17:06:11 +0200

    fix: set perses dashboard manually, fix typo, memoize dashboard skeleton and remove fallback
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>

--- web/src/components/dashboards/shared/dashboard-skeleton.tsx
+++ web/src/components/dashboards/shared/dashboard-skeleton.tsx
@@ -11,6 +11,7 @@ import { CombinedDashboardMetadata } from '../perses/hooks/useDashboardsData';
 import { AllVariableDropdowns } from '../perses/variable-dropdowns';
 import { useIsPerses } from './useIsPerses';
 import { Divider, PageSection, Split, SplitItem, Title } from '@patternfly/react-core';
+import { useDashboardActions } from '@perses-dev/dashboards';
 
 const HeaderTop: React.FC = React.memo(() => {
   const { t } = useTranslation(process.env.I18N_NAMESPACE);
@@ -40,60 +41,69 @@ type MonitoringDashboardsPageProps = React.PropsWithChildren<{
   dashboardName: string;
 }>;
 
-const DashboardSkeleton: React.FC<MonitoringDashboardsPageProps> = ({
-  children,
-  boardItems,
-  changeBoard,
-  dashboardName,
-}) => {
-  const { t } = useTranslation(process.env.I18N_NAMESPACE);
-  const isPerses = useIsPerses();
+const DashboardSkeleton: React.FC<MonitoringDashboardsPageProps> = React.memo(
+  ({ children, boardItems, changeBoard, dashboardName }) => {
+    const { t } = useTranslation(process.env.I18N_NAMESPACE);
+    const isPerses = useIsPerses();
 
-  const { perspective } = usePerspective();
+    const { perspective } = usePerspective();
+    const { setDashboard } = useDashboardActions();
 
-  return (
-    <>
-      {perspective !== 'dev' && (
-        <Helmet>
-          <title>{t('Metrics dashboards')}</title>
-        </Helmet>
-      )}
-      <PageSection hasBodyWrapper={false}>
-        {perspective !== 'dev' && <HeaderTop />}
-        <Split hasGutter isWrappable>
-          {!_.isEmpty(boardItems) && (
-            <SplitItem>
-              <DashboardDropdown
-                items={boardItems}
-                onChange={changeBoard}
-                selectedKey={dashboardName}
-              />
-            </SplitItem>
-          )}
-          {isPerses ? (
-            <AllVariableDropdowns key={dashboardName} />
-          ) : (
-            <LegacyDashboardsAllVariableDropdowns key={dashboardName} />
-          )}
-          {perspective === 'dev' ? (
-            <>
+    const onChangeBoard = (selectedDashboard: string) => {
+      changeBoard(selectedDashboard);
+
+      if (isPerses) {
+        const selectedBoard = boardItems.find((item) => item.name === selectedDashboard);
+        if (selectedBoard) {
+          setDashboard(selectedBoard.persesDashboard);
+        }
+      }
+    };
+
+    return (
+      <>
+        {perspective !== 'dev' && (
+          <Helmet>
+            <title>{t('Metrics dashboards')}</title>
+          </Helmet>
+        )}
+        <PageSection hasBodyWrapper={false}>
+          {perspective !== 'dev' && <HeaderTop />}
+          <Split hasGutter isWrappable>
+            {!_.isEmpty(boardItems) && (
+              <SplitItem>
+                <DashboardDropdown
+                  items={boardItems}
+                  onChange={onChangeBoard}
+                  selectedKey={dashboardName}
+                />
+              </SplitItem>
+            )}
+            {isPerses ? (
+              <AllVariableDropdowns key={dashboardName} />
+            ) : (
+              <LegacyDashboardsAllVariableDropdowns key={dashboardName} />
+            )}
+            {perspective === 'dev' ? (
+              <>
+                <SplitItem isFilled />
+                <SplitItem>
+                  <TimespanDropdown />
+                </SplitItem>
+                <SplitItem>
+                  <PollIntervalDropdown />
+                </SplitItem>
+              </>
+            ) : (
               <SplitItem isFilled />
-              <SplitItem>
-                <TimespanDropdown />
-              </SplitItem>
-              <SplitItem>
-                <PollIntervalDropdown />
-              </SplitItem>
-            </>
-          ) : (
-            <SplitItem isFilled />
-          )}
-        </Split>
-      </PageSection>
-      <Divider />
-      {children}
-    </>
-  );
-};
+            )}
+          </Split>
+        </PageSection>
+        <Divider />
+        {children}
+      </>
+    );
+  },
+);
 
 export default DashboardSkeleton;
