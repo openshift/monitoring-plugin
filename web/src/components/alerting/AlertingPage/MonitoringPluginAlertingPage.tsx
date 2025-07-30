f2a35140ccfa0d126586df171d6ba8c52b45ca7c
PeterYurkovich <pyurkovi@redhat.com>
2025-07-30 16:08:09 -0400

    refactor: expose multiple entrypoints in same module

--- web/src/components/alerting/AlertingPage/MonitoringPluginAlertingPage.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import AlertingPage from './AlertingPage';
-
-const MonitoringPluginAlertingPage: React.FC = () => <AlertingPage plugin="monitoring-plugin" />;
-
-export default MonitoringPluginAlertingPage;
