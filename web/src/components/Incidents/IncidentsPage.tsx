7324fe0174e3cb7c33f03e2ce248feb001043a28
Tomáš Remeš <tremes@redhat.com>
2025-08-26 14:31:58 +0200

    fix: rename the incident metrics

--- web/src/components/Incidents/IncidentsPage.tsx
+++ web/src/components/Incidents/IncidentsPage.tsx
@@ -221,7 +221,7 @@ const IncidentsPage = () => {
           const response = await fetchDataForIncidentsAndAlerts(
             safeFetch,
             range,
-            'cluster:health:components:map',
+            'cluster_health_components_map',
             perspective,
           );
           return response.data.result;
@@ -258,7 +258,7 @@ const IncidentsPage = () => {
           const response = await fetchDataForIncidentsAndAlerts(
             safeFetch,
             range,
-            `cluster:health:components:map{group_id='${incidentGroupId}'}`,
+            `cluster_health_components_map{group_id='${incidentGroupId}'}`,
             perspective,
           );
           return response.data.result;
