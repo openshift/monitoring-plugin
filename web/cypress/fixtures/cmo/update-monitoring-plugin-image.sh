28cb4b7693736cd16d8bc5988327ca8fff0cf9e5
Evelyn Tanigawa Murasaki <emurasak@redhat.com>
2025-09-12 15:48:02 -0300

    fix mp injection

--- web/cypress/fixtures/cmo/update-monitoring-plugin-image.sh
+++ web/cypress/fixtures/cmo/update-monitoring-plugin-image.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
 
 # Generate a random filename
-RANDOM_FILE="/tmp/cmo_monitoring_csv_$(date +%s%N).yaml"
+MONITORING_FILE="/tmp/monitoring_csv_$(date +%s%N).yaml"
 
-CMO_CSV_NAME=$(oc get deployment --kubeconfig "${KUBECONFIG}" --namespace="${MP_NAMESPACE}" | grep "cluster-monitoring-operator" | awk '{print $1}')
+MONITORING_CSV_NAME=$(oc get deployment --kubeconfig "${KUBECONFIG}" --namespace="${MP_NAMESPACE}" | grep "monitoring-plugin" | awk '{print $1}')
 
 oc project --namespace="${MP_NAMESPACE}"
 
-oc get deployment "${CMO_CSV_NAME}" -n "${MP_NAMESPACE}" -o yaml > "${RANDOM_FILE}" --kubeconfig "${KUBECONFIG}"
+oc get deployment "${MONITORING_CSV_NAME}" -n "${MP_NAMESPACE}" -o yaml > "${MONITORING_FILE}" --kubeconfig "${KUBECONFIG}"
 
 # Patch the deployment file related images
-sed -i "s#value: .*monitoring-plugin.*#value: ${MP_IMAGE}#g" "${RANDOM_FILE}"
-sed -i "s#^\([[:space:]]*- -images=monitoring-plugin=\).*#\1${MP_IMAGE}#g" "${RANDOM_FILE}"
+sed -i "s#^\([[:space:]]*image: \).*#\1${MP_IMAGE}#g" "${MONITORING_FILE}"
 
-oc replace -f "${RANDOM_FILE}" --kubeconfig "${KUBECONFIG}"
+oc replace -f "${MONITORING_FILE}" --kubeconfig "${KUBECONFIG}"
 
-# Scale down
+# Scale down the cluster-monitoring-operator as "manager" pod is responsible for the monitoring-plugin
 oc patch clusterversion version --type json -p "$(cat disable-monitoring.yaml)"
 
 oc scale --replicas=0 -n "${MP_NAMESPACE}" deployment/cluster-monitoring-operator
 
-oc scale --replicas=0 -n "${MP_NAMESPACE}" deployment/monitoring-plugin
-
 # Apply the patched deployment resource file
-oc replace -f "${RANDOM_FILE}" --kubeconfig "${KUBECONFIG}"
+oc replace -f "${MONITORING_FILE}" --kubeconfig "${KUBECONFIG}"
 
-# Scale up the Monitoring-plugin
-oc scale --replicas=1 -n "${MP_NAMESPACE}" deployment/cluster-monitoring-operator
-
+# Scale up the monitoring-plugin with new image, leaving the cluster-monitoring-operator at 0 replicas to not revert the change
 oc scale --replicas=1 -n "${MP_NAMESPACE}" deployment/monitoring-plugin
 
 # Wait for the operator to reconcile the change and make sure all the pods are running.
 sleep 30
 OUTPUT=`oc wait --for=condition=Ready pods --selector=app.kubernetes.io/part-of=monitoring-plugin -n "${MP_NAMESPACE}" --timeout=60s`
-echo "${OUTPUT}"
-OUTPUT=`oc wait --for=condition=ready pods -l app.kubernetes.io/name=cluster-monitoring-operator -n "${MP_NAMESPACE}" --timeout=60s --kubeconfig "${KUBECONFIG}"`
 echo "${OUTPUT}"
