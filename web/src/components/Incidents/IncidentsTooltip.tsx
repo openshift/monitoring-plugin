c8e73395abe2a74f7a6299e919d8e74d7b6e606d
Alberto Falossi <afalossi@redhat.com>
2025-10-06 16:20:10 +0200

    fix: fix tooltip vertical position

--- web/src/components/Incidents/IncidentsTooltip.tsx
+++ web/src/components/Incidents/IncidentsTooltip.tsx
@@ -7,18 +7,17 @@ export const IncidentsTooltip = ({
   x,
   y,
   x0,
-  height,
   text,
 }: {
   x?: number;
   y?: number;
   x0?: number;
-  height?: number;
   text?: string | Array<string>;
 }) => {
-  const posx = x - (x - x0) / 2 - TOOLTIP_MAX_WIDTH / 2;
-  const posy = y - Math.min(height || 0, TOOLTIP_MAX_HEIGHT) / 2 - 20;
   const textArray: Array<string> = Array.isArray(text) ? text : [text];
+  const posx = x - (x - x0) / 2 - TOOLTIP_MAX_WIDTH / 2;
+  const tooltipHeight = textArray.length === 6 ? 165 : 145;
+  const posy = y - tooltipHeight;
 
   return (
     <VictoryPortal>
