755e8c29644c433bba818d6fdd36ef79a2508706
David Rajnoha <drajnoha@redhat.com>
2025-12-11 14:00:18 +0100

    fix: COO install bugs in Incident tests
    The current implementation was using the COO namespace there.
    Additionally adds wait after the initial welcome pop-up removal to
    target a race condition when cypress navigated to a new page before pop
    closing finished, leading to it rerendering again.

--- web/cypress/views/tour.ts
+++ web/cypress/views/tour.ts
@@ -4,6 +4,8 @@ export const guidedTour = {
         if ($body.find(`[data-test="guided-tour-modal"]`).length > 0) {
           cy.byTestID('tour-step-footer-secondary').contains('Skip tour').click();
         }
+        // Prevents navigating away from the page before the tour is closed
+        cy.wait(2000);
       });
     },
 
@@ -12,6 +14,8 @@ export const guidedTour = {
         if ($body.find(`[aria-label="Welcome modal"]`).length > 0) {
           cy.get('[aria-label="Close"]').should('be.visible').click();
         }
+        // Prevents navigating away from the page before the tour is closed
+        cy.wait(2000);
       });
     },
   };
