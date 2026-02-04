97375a39e29c1d3148248e0472143903764c5452
Aviv Litman <64130977+avlitman@users.noreply.github.com>
2026-02-04 15:12:04 +0200

    Re-add missing metav1 import (#15)
    Co-authored-by: Aviv Litman <alitman@alitman-thinkpadp1gen7.raanaii.csb>

--- pkg/k8s/relabeled_rules.go
+++ pkg/k8s/relabeled_rules.go
@@ -16,6 +16,7 @@ import (
 	"github.com/prometheus/prometheus/model/relabel"
 	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
