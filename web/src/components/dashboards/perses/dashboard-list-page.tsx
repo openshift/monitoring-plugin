dda7a15136237ce625fb08f8c83eaea87695df49
Jenny Zhu <jenny.a.zhu@gmail.com>
2026-03-16 16:42:46 -0400

    fix: OU-1271 Upgrade React Router because console supports React Router 7 and conflicts with react router 5

--- web/src/components/dashboards/perses/dashboard-list-page.tsx
+++ web/src/components/dashboards/perses/dashboard-list-page.tsx
@@ -1,7 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { type FC } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { type FC } from 'react';
 import { QueryParamProvider } from 'use-query-params';
-import { ReactRouter5Adapter } from 'use-query-params/adapters/react-router-5';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { DashboardList } from './dashboard-list';
 import { ToastProvider } from './ToastProvider';
 
@@ -16,7 +17,7 @@ const queryClient = new QueryClient({
 
 const DashboardListPage: FC = () => {
   return (
-    <QueryParamProvider adapter={ReactRouter5Adapter}>
+    <QueryParamProvider adapter={ReactRouter6Adapter}>
       <QueryClientProvider client={queryClient}>
         <ToastProvider>
           <DashboardList />
