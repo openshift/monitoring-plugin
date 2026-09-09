40b2c0be0175a0bfdcdec97da24eb9b2f3b26e0f
Daniel Chromik <dchromik@redhat.com>
2026-09-09 13:31:02 +0200

    feat: added URL validation to alert details

--- web/src/features/alerts/pages/AlertsDetailsPage.tsx
+++ web/src/features/alerts/pages/AlertsDetailsPage.tsx
@@ -87,6 +87,7 @@ import {
   usePerspective,
 } from '@/shared/hooks/usePerspective';
 import { MonitoringState } from '@/shared/store/store';
+import { getSafeExternalURL } from '@/shared/utils/utils';
 import { AlertResource, alertState, RuleResource } from '@/shared/utils/utils';
 
 const AlertsDetailsPage_: FC = () => {
@@ -121,7 +122,7 @@ const AlertsDetailsPage_: FC = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const labels: PrometheusLabels = useMemo(() => alert?.labels, [labelsMemoKey]);
 
-  const runbookURL = alert?.annotations?.runbook_url;
+  const runbookURL = getSafeExternalURL(alert?.annotations?.runbook_url);
 
   const sourceId = rule?.sourceId;
 
