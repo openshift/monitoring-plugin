38617003905c1879edb2d0bc65598b9e1e8c0900
Daniel Chromik <dchromik@redhat.com>
2026-09-15 12:37:23 +0200

    OU-1488: move URL validation into ExternalLink

--- web/src/components/alerting/AlertRulesDetailsPage.tsx
+++ web/src/components/alerting/AlertRulesDetailsPage.tsx
@@ -63,7 +63,7 @@ import {
 import KebabDropdown from '../kebab-dropdown';
 import { Labels } from '../labels';
 import { ToggleGraph } from '../MetricsPage';
-import { alertDescription, getSafeExternalURL, RuleResource } from '../utils';
+import { alertDescription, RuleResource } from '../utils';
 import { MonitoringProvider } from '../../contexts/MonitoringContext';
 
 import { DataTestIDs } from '../data-test';
@@ -167,7 +167,7 @@ const AlertRulesDetailsPage_: FC = () => {
   };
 
   // eslint-disable-next-line camelcase
-  const runbookURL = getSafeExternalURL(rule?.annotations?.runbook_url);
+  const runbookURL = rule?.annotations?.runbook_url;
 
   return (
     <>
